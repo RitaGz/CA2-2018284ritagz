de91d51915d5d05f313b0cf6a7301546b07cccc4
RitaGz <rita_22jaja@hotmail.com>
2021-04-03 19:59:34 +0000

    ritaCaffee update

--- app.js
+++ app.js
@@ -4,11 +4,31 @@ var http = require('http'), //This module provides the HTTP server functionaliti
     fs = require('fs'), //This module allows to work witht the file system: read and write files back
     xmlParse = require('xslt-processor').xmlParse, //This module allows us to work with XML files
     xsltProcess = require('xslt-processor').xsltProcess; //The same module allows us to utilise XSL Transformations
+    xml2js = require('xml2js'); //This module does XML to JSON conversion and also allows us to get from JSON back to XML
 
 var router = express(); //We set our routing to be handled by Express
 var server = http.createServer(router); //This is where our server gets created
 
 router.use(express.static(path.resolve(__dirname,'views'))); //we serve static content from views folder
+
+
+// Function to read in XML file and convert it to JSON
+function xmlFileToJs(filename, cb) {
+  var filepath = path.normalize(path.join(__dirname, filename));
+  fs.readFile(filepath, 'utf8', function(err, xmlStr) {
+    if (err) throw (err);
+    xml2js.parseString(xmlStr, {}, cb);
+  });
+}
+//Function to convert JSON to XML and save it
+function jsToXmlFile(filename, obj, cb) {
+  var filepath = path.normalize(path.join(__dirname, filename));
+  var builder = new xml2js.Builder();
+  var xml = builder.buildObject(obj);
+  fs.unlinkSync(filepath);
+  fs.writeFile(filepath, xml, cb);
+}
+
 
 router.get('/', function(req, res) {
 
@@ -19,9 +39,15 @@ router.get('/', function(req, res) {
     
     var doc = xmlParse(xml); //Parsing our XML file
     var stylesheet = xmlParse(xsl); //Parsing our XSL file
+    
     var result = xsltProcess(doc, stylesheet); //This does our XSL Transformation
     
-      res.end(result.toString()); //Send the result back to the user, but convert to type string first
+    xmlFileToJs('RitasCoffee.xml', function(err, result) {
+        if (err) throw (err);
+        console.log(result);   //or result.toString() 
+    });
+    
+    res.end(result.toString()); //Send the result back to the user, but convert to type string first
 });
 
 server.listen(process.env.PORT || 3000, process.env.IP || "0.0.0.0", function () {
