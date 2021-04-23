adf47f403c731a0690eb1a27de388caecabf817c
RitaGz <rita_22jaja@hotmail.com>
2021-04-23 20:47:42 +0000

    23/04/21

--- app.js
+++ app.js
@@ -114,8 +114,19 @@ server.listen(process.env.PORT || 3000, process.env.IP || "0.0.0.0", function ()
 });
 
 //this part of code was added for the CA2
-const http = require('http');
-http.createServer((req, res)=>{
-res.write("Hello world \n"); // write a response
-res.end(); //end the response
-}).listen(8000); // listen for requests on port 8000
+// const http = require('http');
+// http.createServer((req, res)=>{
+// res.write("Hello world \n"); // write a response
+// res.end(); //end the response
+// }).listen(8000); // listen for requests on port 8000
+
+// let users = []; // names of users will be stored here
+// axios.get("https://jsonplaceholder.typicode.com/users")
+// .then(({ data }) => {
+// users = data.map(user => user.name); // get only the names of the users and store in an array
+//     console.log(users)
+// })
+    
+// .catch(error=>{
+// console.log(error);
+// });
